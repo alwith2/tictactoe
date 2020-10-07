34e2a54ff73946099525e0a9d839526331356900
Elizabeth Withers <elizabethwithers@MLA-iPhone.localdomain>
2020-10-07 16:02:31 -0400

    add files

---
+++ pseudocode.md
@@ -0,0 +1,16 @@
+## create game board using js
+create  3x3 grid
+grid cells should display correct info {
+    x and o
+    display who won
+    display if draw
+}
+createResetButton () {
+createElement div
+}
+game flow {
+    enable clicks
+    track clicks 
+    check if move is valid
+    update game state
+}
---
+++ scratchpad.js
@@ -0,0 +1,20 @@
+let board = [
+    ['','',''],
+    ['','',''],
+    ['','',''],
+];
+let player1 = 'X';
+let player 2 = 'O';
+
+function setup() {
+    createBoard(400,400);
+}
+
+function play(){
+    background(220);
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0: j < 3; j++) {
+
+        }
+    }
+}
